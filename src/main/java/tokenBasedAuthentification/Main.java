02eab6a2659c9db5bcf23c1531c983833be8a605
Journerist <akrillo89@gmail.com>
2016-04-17 18:40:10 +0200

    removed unused auth intercepter

--- src/main/java/tokenBasedAuthentification/Main.java
+++ src/main/java/tokenBasedAuthentification/Main.java
@@ -3,7 +3,6 @@ package tokenBasedAuthentification;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-import tokenBasedAuthentification.provider.AuthSecurityInterceptor;
 import tokenBasedAuthentification.provider.TransactionalBeginIntercepter;
 import tokenBasedAuthentification.provider.TransactionalEndIntercepter;
 
@@ -28,7 +27,6 @@ public class Main {
     public static HttpServer startServer(int port) {
         // create a resource config that scans for JAX-RS resources and providers
         final ResourceConfig rc = new ResourceConfig().packages("tokenBasedAuthentification.rest");
-        rc.register(AuthSecurityInterceptor.class);
         rc.register(TransactionalBeginIntercepter.class);
         rc.register(TransactionalEndIntercepter.class);
 
